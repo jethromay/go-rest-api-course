0109845329b9dcbc5dbc0e36522b520135061c9d
Jethro May <jethro.may@minttwist.com>
2021-04-03 13:35:55 +0200

    Added first API route

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,15 +2,26 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+
+	transportHTTP "github.com/jethromay/go-rest-api-course/internal/transport/http"
 )
 
 // App - Struct which contains pointers and other things like db connections.
-type App struct {
-}
+type App struct{}
 
 // Run - Sets up application
 func (app *App) Run() error {
 	fmt.Println("Setting up our APP")
+
+	handler := transportHTTP.NewHandler()
+	handler.SetupRoutes()
+
+	if err := http.ListenAndServe(":9000", handler.Router); err != nil {
+		fmt.Println("Failed to setup server")
+		return err
+	}
+
 	return nil
 }
 
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/jethromay/go-rest-api-course
 
 go 1.15
+
+require github.com/gorilla/mux v1.8.0
